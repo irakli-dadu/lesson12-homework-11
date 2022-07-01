f0c04fb7658fa9b8238aadf6c51459139a337b01
irakli-dadu <lipsowow@gmail.com>
2022-07-01 22:01:49 +0400

    tryharding Js
    u need the nerves of steel xD

---
+++ script.js
@@ -0,0 +1,63 @@
+let maso = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+
+// console.log(maso[0], maso[1], maso[5], maso[6]);
+
+
+
+
+let jero = [32, 10, 'hello', null, 'hello2', 50]
+for (let x of jero) {
+    if (x % 5 == 0) {
+        
+    }
+}
+
+
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89]
+
+for (let y of numbers) {
+    if (y>5) {
+        
+    }
+}
+
+
+let user =  {
+    firstname: 'giorgi',
+    lastname: 'smith',
+    age: 25,
+    studentstatus: active
+}
+console.log(user.studentstatus);
+
+
+
+
+
+let Auser = {
+    name: 'giorgi',
+    age: 20,
+    studentstatus: active,
+    comments: [
+        {text: 'hello'},
+        {text: 'hello gio'},
+        {text: 'hello world'}
+    ]
+}
+If (Auser.age > 18, Auser.studentstatus = active) {
+    console.log(Auser.comments[0].text);
+}
+if (Auser.name = 'giorgi') {
+    console.log(Auser.comments[1].text);
+}
+if (Auser.studentstatus = active || Auser.age < 25) {
+    console.log(Auser.comments[2].text);
+} else {
+    console.log('error');
+}
+
+// ar vici ras vaketeb arasworad errors migdebs 
+
+
+
+
